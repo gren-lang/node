79f5b9f67a52004909b0faf3568b5e9baae1f0dc
Robin Heggelund Hansen <glad.sand2211@heghan.org>
2022-08-26 12:59:44 +0200

    Use correct syntax in kernel code, and depend on Platform.export

--- src/Gren/Kernel/Node.js
+++ src/Gren/Kernel/Node.js
@@ -1,8 +1,9 @@
 /*
-
+import Gren.Kernel.Platform exposing (export)
 */
 
-function __log(text) {
-    console.log(text);
-    return {};
-}
+var _Node_log = F2(function(text, args) {
+  // NOTE: this function needs __Platform_export available to work
+  console.log(text);
+  return {};
+});
