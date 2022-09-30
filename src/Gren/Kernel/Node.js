6195159684f29f40ba2161768ec1d48516ab4e74
Robin Heggelund Hansen <glad.sand2211@heghan.org>
2022-09-30 13:23:53 +0200

    Add and run prettier.

--- src/Gren/Kernel/Node.js
+++ src/Gren/Kernel/Node.js
@@ -3,28 +3,25 @@ import Gren.Kernel.Platform exposing (export)
 import Gren.Kernel.Scheduler exposing (binding, succeed)
 */
 
+var process = require("process");
 
-var process = require('process');
-
-
-var _Node_log = F2(function(text, args) {
+var _Node_log = F2(function (text, args) {
   // NOTE: this function needs __Platform_export available to work
   console.log(text);
   return {};
 });
 
-
 var _Node_initialize = __Scheduler_binding(function (callback) {
-    return callback(__Scheduler_succeed({
-        __$args: process.argv,
-        __$stdout: process.stdout,
-        __$stderr: process.stderr
-    }));
+  return callback(
+    __Scheduler_succeed({
+      __$args: process.argv,
+      __$stdout: process.stdout,
+      __$stderr: process.stderr,
+    })
+  );
 });
 
-
-var _Node_sendToStream = F2(function(stream, text) {
-    stream.write(text);
-    return {};
+var _Node_sendToStream = F2(function (stream, text) {
+  stream.write(text);
+  return {};
 });
-
