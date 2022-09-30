66d3483a7c2e3b2ebc27a35377b5618917758975
Robin Heggelund Hansen <glad.sand2211@heghan.org>
2022-09-30 13:07:04 +0200

    Initial support for streams.

--- src/Gren/Kernel/Node.js
+++ src/Gren/Kernel/Node.js
@@ -1,9 +1,30 @@
 /*
 import Gren.Kernel.Platform exposing (export)
+import Gren.Kernel.Scheduler exposing (binding, succeed)
 */
+
+
+var process = require('process');
+
 
 var _Node_log = F2(function(text, args) {
   // NOTE: this function needs __Platform_export available to work
   console.log(text);
   return {};
 });
+
+
+var _Node_initialize = __Scheduler_binding(function (callback) {
+    return callback(__Scheduler_succeed({
+        __$args: process.argv,
+        __$stdout: process.stdout,
+        __$stderr: process.stderr
+    }));
+});
+
+
+var _Node_sendToStream = F2(function(stream, text) {
+    stream.write(text);
+    return {};
+});
+
