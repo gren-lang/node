0f365d9f29191701f7c9fc81b87d86207e3163c6
Robin Heggelund Hansen <glad.sand2211@heghan.org>
2022-09-30 13:30:05 +0200

    Pass env variables as part of node initialization.

--- src/Gren/Kernel/Node.js
+++ src/Gren/Kernel/Node.js
@@ -1,6 +1,9 @@
 /*
+
+import Dict exposing (empty, insert)
 import Gren.Kernel.Platform exposing (export)
 import Gren.Kernel.Scheduler exposing (binding, succeed)
+
 */
 
 var process = require("process");
@@ -15,6 +18,7 @@ var _Node_initialize = __Scheduler_binding(function (callback) {
   return callback(
     __Scheduler_succeed({
       __$args: process.argv,
+      __$env: _Node_objToDict(process.env),
       __$stdout: process.stdout,
       __$stderr: process.stderr,
     })
@@ -25,3 +29,13 @@ var _Node_sendToStream = F2(function (stream, text) {
   stream.write(text);
   return {};
 });
+
+function _Node_objToDict(obj) {
+  var dict = __Dict_empty;
+
+  for (var key in obj) {
+    dict = A3(__Dict_insert, key, obj[key], dict);
+  }
+
+  return dict;
+}
