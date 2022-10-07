5dd03009f21d770d1f764b278c4adcef58b0b2fe
Robin Heggelund Hansen <glad.sand2211@heghan.org>
2022-10-07 14:39:49 +0200

    Add Terminal module for getting information about potentially attached terminals.

---
+++ src/Gren/Kernel/Terminal.js
@@ -0,0 +1,43 @@
+/*
+
+import Gren.Kernel.Scheduler exposing (binding, succeed, rawSpawn)
+
+*/
+
+var process = require("process");
+
+var _Terminal_init = __Scheduler_binding(function (callback) {
+  callback(
+    __Scheduler_succeed({
+      __$isTTY: process.stdout.isTTY,
+      __$colorDepth: process.stdout.getColorDepth(),
+      __$columns: process.stdout.columns,
+      __$rows: process.stdout.rows,
+    })
+  );
+});
+
+var _Terminal_attachListener = function (sendToApp) {
+  return __Scheduler_binding(function (_callback) {
+    var listener = function (data) {
+      __Scheduler_rawSpawn(
+        sendToApp({
+          __$columns: process.stdout.columns,
+          __$rows: process.stdout.rows,
+        })
+      );
+    };
+
+    process.stdout.on("resize", listener);
+
+    return function () {
+      process.stdout.off("resize", listener);
+      process.stdout.pause();
+    };
+  });
+};
+
+var _Terminal_setStdInRawMode = function (toggle) {
+  process.stdin.setRawMode(toggle);
+  return {};
+};
