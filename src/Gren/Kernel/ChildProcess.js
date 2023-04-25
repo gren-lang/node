6ff4ed7105e93a9b512327caac6599bafcb33321
Robin Heggelund Hansen <git@heghan.org>
2023-04-25 17:45:01 +0200

    Pass JS friendly values to kernel function.

---
+++ src/Gren/Kernel/ChildProcess.js
@@ -0,0 +1,13 @@
+/*
+
+import Gren.Kernel.Scheduler exposing (binding, succeed, fail)
+
+*/
+
+var bufferNs = require("node:buffer");
+var childProcess = require("node:child_process");
+
+var _ChildProcess_run = function (options) {
+  return __Scheduler_binding(function (callback) {
+  });
+});
