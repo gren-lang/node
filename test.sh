bf01f62dc8674dee7f92f5ad1564ad819891af9c
Robin Heggelund Hansen <git@heghan.org>
2024-01-21 10:15:43 +0100

    Fix test runner.

--- test.sh
+++ test.sh
@@ -2,8 +2,17 @@
 
 set -e
 
-echo -e "Running unit tests...\n" && \
-cd tests && gren make --optimize src/Main.gren && node app && \
+echo -e "Running unit tests...\n"
+pushd tests
+gren make --optimize src/Main.gren && node app
+popd
 
-echo -e "\n\nRunning http-server examples...\n" && \
-cd examples/http-server && make test
+echo -e "\n\nRunning http-server examples...\n"
+pushd examples/http-server
+make test
+popd
+
+echo -e "\n\nRunning http-client examples...\n"
+pushd examples/http-client
+npm test
+popd
