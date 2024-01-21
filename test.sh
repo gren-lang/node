f0f201b756d5e28a24af2df391b8dc1beceddba2
Robin Heggelund Hansen <git@heghan.org>
2024-01-21 13:04:43 +0100

    Remove HttpServer unit tests.

--- test.sh
+++ test.sh
@@ -2,17 +2,12 @@
 
 set -e
 
-echo -e "Running unit tests...\n"
-pushd tests
-gren make --optimize src/Main.gren && node app
-popd
-
 echo -e "\n\nRunning http-server examples...\n"
 pushd examples/http-server
-make test
+make test || exit 1
 popd
 
 echo -e "\n\nRunning http-client examples...\n"
 pushd examples/http-client
-npm test
+npm test || exit 1
 popd
