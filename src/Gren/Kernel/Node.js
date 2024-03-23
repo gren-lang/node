190303c05b7ae4ce234ee1278820096d76bdf936
Robin Heggelund Hansen <git@heghan.org>
2024-03-23 07:22:26 +0100

    Removed unused kernel function.

--- src/Gren/Kernel/Node.js
+++ src/Gren/Kernel/Node.js
@@ -1,18 +1,11 @@
 /*
 
 import Dict exposing (empty, insert)
-import Gren.Kernel.Platform exposing (export)
 import Gren.Kernel.Scheduler exposing (binding, succeed)
 
 */
 
 var process = require("node:process");
-
-var _Node_log = F2(function (text, args) {
-  // NOTE: this function needs __Platform_export available to work
-  console.log(text);
-  return {};
-});
 
 var _Node_init = __Scheduler_binding(function (callback) {
   callback(
