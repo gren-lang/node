f4bba703b955eabe59c29079040fcb9b3d85fd62
Robin Heggelund Hansen <git@heghan.org>
2024-03-29 08:03:37 +0100

    Add toString functions for FileSystem.Path

--- src/Gren/Kernel/FilePath.js
+++ src/Gren/Kernel/FilePath.js
@@ -26,3 +26,31 @@ var _FilePath_parse = function (pathMod, str) {
     __$root: result.root,
   };
 };
+
+var _FilePath_toPosix = function (filePath) {
+  if (filePath.__$root !== "" && filePath.__$root !== "/") {
+    filePath = { ...filePath, __$root: "/" };
+  }
+
+  return _FilePath_format(path.posix, filePath);
+};
+
+var _FilePath_toWin32 = function (filePath) {
+  return _FilePath_format(path.win32, filePath);
+};
+
+var _FilePath_format = function (pathMod, filePath) {
+  const filename =
+    filePath.__$extension.length > 0
+      ? filePath.__$filename + "." + filePath.__$extension
+      : filePath.__$filename;
+
+  let pathArray = null;
+  if (filename === "") {
+    pathArray = filePath.__$directory;
+  } else {
+    pathArray = filePath.__$directory.concat(filename);
+  }
+
+  return filePath.__$root + pathArray.join(pathMod.sep);
+};
