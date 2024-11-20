fe3ec2c611affb82ae98c73290be990a9af2e46f
Robin Heggelund Hansen <git@heghan.org>
2024-11-20 21:15:40 +0100

    Fix variable name.

--- src/Gren/Kernel/FileSystem.js
+++ src/Gren/Kernel/FileSystem.js
@@ -651,7 +651,7 @@ var _FileSystem_writeFile = F2(function (data, path) {
   });
 });
 
-var _FileSystem_writeFileStream = function (data) {
+var _FileSystem_writeFileStream = function (path) {
   return __Scheduler_binding(function (callback) {
     fs.createWritableStream(__FilePath_toString(path), function (err, fstream) {
       if (err) {
